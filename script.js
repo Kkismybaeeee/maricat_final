b0c1bce7e94a43634d8e25d40f92499e28b93f78
Mouuuu! <120558065+Kkismybaeeee@users.noreply.github.com>
2024-06-18 17:07:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-var buildUrl = "Build";
-var loaderUrl = buildUrl + "/Maricat.loader.js";
-var config = {
-    dataUrl: buildUrl + "/Maricat.data",
-    frameworkUrl: buildUrl + "/Maricat.framework.js",
-    codeUrl: buildUrl + "/Maricat.wasm",
-    streamingAssetsUrl: "StreamingAssets",
-    companyName: "DefaultCompany",
-    productName: "Maricat",
-    productVersion: "1.0",
-};
-
-var container = document.querySelector("#unityContainer");
-var canvas = document.createElement("canvas");
-container.appendChild(canvas);
-
-var loadingBar = document.createElement("div");
-loadingBar.id = "loadingBar";
-var loadingBarBackground = document.createElement("div");
-loadingBarBackground.id = "loadingBarBackground";
-loadingBar.appendChild(loadingBarBackground);
-container.appendChild(loadingBar);
-
-var script = document.createElement("script");
-script.src = loaderUrl;
-script.onload = () => {
-    createUnityInstance(canvas, config, (progress) => {
-        loadingBarBackground.style.width = 100 * progress + "%";
-    }).then((unityInstance) => {
-        loadingBar.remove();
-    }).catch((message) => {
-        alert(message);
-    });
-};
-document.body.appendChild(script);
-
 // JavaScript for navigation and setting selected state
 document.getElementById('gamecenter').addEventListener('click', function() {
     window.location.href = 'index.html';
